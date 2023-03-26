df5402f4ff9c5cffbb2d900ee86bf26357f0be9c
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-03-26 11:52:56 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,5 @@
 script_location=$(pwd)
-##
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
@@ -13,6 +13,8 @@ npm install
 
 cp $script_location/files/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
 
 cp $script_location/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
