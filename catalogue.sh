2cd28f9df3af4abc2f5f1748bd78694670ce330a
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-03-26 11:20:33 +0530

    nginx script

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,5 @@
 script_location=$(pwd)
-
+##
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 script_location=$(pwd)
-
+##
 cp $script_location/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
