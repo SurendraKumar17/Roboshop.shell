ac9f5b7d019a75913db50184ebbd56c72e2838b2
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-07 11:41:53 +0530

    common.sh file for printing status of commands and Etc

---
+++ files/common.sh
@@ -0,0 +1,13 @@
+script_location=$(pwd)
+LOG=/tmp/roboshop.log
+
+status_check() {
+  if  [ $? -eq 0 ]; then
+      echo -e "\e[1;32mSUCCESS\e[0m"
+  else
+    echo -e "\e[1;31mFAILURE\e[0m"
+    echo "Refer log file for more info , LOG - ${LOG}"
+    exit
+    fi
+
+}
