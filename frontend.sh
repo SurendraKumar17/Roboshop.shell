7022620624a61fdc6b650123d29d82d7f0bff8f2
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-08 13:10:00 +0530

    Frontend

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,36 @@
-script_location=$(pwd)
+source common.sh
 
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
+print_head "Install Nginx"
+yum install nginx -y &>>${LOG}
+status_check
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+print_head "Remove Nginx old content"
+rm -rf /usr/share/nginx/html/* &>>${LOG}
+status_check
+
+print_head "Download frontend content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+status_check
+
+cd /usr/share/nginx/html &>>${LOG}
+
+print_head "Extract frontend content"
+unzip /tmp/frontend.zip &>>${LOG}
+status_check
 
 ##As we dont have VI editor to edit the files, we create the file locally and copy the location to where our scripts are exist.
+print_head "Copy roboshop nginx conf file"
+cp $script_location/files/nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+status_check
 
-cp $script_location/files/nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf
+print_head "Enable Nginx "
+systemctl enable nginx &>>${LOG}
+status_check
 
-systemctl restart nginx
+print_head "Start Nginx"
+systemctl start nginx &>>${LOG}
+status_check
+
+print_head " Restart nginx"
+systemctl restart nginx &>>${LOG}
+status_check
