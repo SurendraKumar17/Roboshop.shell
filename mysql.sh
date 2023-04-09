844bb3b7cb23161f5a80e15120838be091937502
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-09 19:43:38 +0530

    .

---
+++ mysql.sh
@@ -0,0 +1,30 @@
+source common.sh
+
+if [ -z "$root_mysql_password"]; then
+  echo "variable root_mysql_password is missing"
+  exit
+fi
+
+print_head "Disable default mysql module"
+dnf module disable mysql -y &>>${LOG}
+status_check
+
+print_head "Copy mysql Repo file"
+cp $script_location/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+status_check
+
+print_head "Install mySQL server"
+yum install mysql-community-server -y &>>${LOG}
+status_check
+
+print_head "Enable mySQL"
+systemctl enable mysqld &>>${LOG}
+status_check
+
+print_head "Restart mysql"
+systemctl restart mysqld &>>${LOG}
+status_check
+
+print_head "Reset default database password"
+mysql_secure_installation --set-root-pass $root_mysql_password &>>${LOG}
+status_check
