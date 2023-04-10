9d9ec88cba2f6e14bbeacb1ce30e6bca9cf19cd2
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-10 19:19:59 +0530

    RabbitMQ server

---
+++ rabbit.sh
@@ -0,0 +1,42 @@
+Source common.sh
+
+if [ -z "$roboshop_rabbitmq_password"]; then
+  echo "variable roboshop_rabbitmq_password is missing"
+  exit
+fi
+
+print_head "Configuring Erlang YUM repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
+status_check
+
+print_head "Install Erlang"
+yum install erlang -y &>>${LOG}
+status_check
+
+print_head "Configuring RabbitMQ YUM repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
+status_check
+
+print_head "Install RabbitMQ"
+yum install rabbitmq-server -y &>>${LOG}
+status_check
+
+print_head "Enable rabbitmq server"
+systemctl enable rabbitmq-server &>>${LOG}
+status_check
+
+print_head "Start rabbitmq server"
+systemctl start rabbitmq-server &>>${LOG}
+status_check
+
+print_head "Add application user"
+rabbitmqctl add_user roboshop $roboshop_rabbitmq_password &>>${LOG}
+status_check
+
+print_head "Add tags to application user"
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG}
+status_check
+
+print_head "add permissions to application user"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
+status_check
